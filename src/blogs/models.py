ce96e259539784b8f18841915c8e7c6158f06afa
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2024-05-04 18:31:07 +0300

    users can now delete their account

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Blog(models.Model):
     """Model for student organizations"""
 
     title = models.CharField(max_length=50, default="")
     content = models.EmailField(max_length=100, default="")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
--- src/blogs/views.py
+++ src/blogs/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from .models import Blog
 
 @login_required
 def frontPageView(request):
     """Render the front page of the application"""
 
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.filter(Q(author=request.user))
 
-    return render(request, 'frontpage.html', {'blogs' : blogs})
+    return render(request, 'frontpage.html', {'blogs' : blogs, 'user' : request.user})
 
 def createBlogView(request):
     """Create a new blog"""
@@ -21,7 +22,8 @@ def createBlogView(request):
         if 1000 > len(blog_content) > 0:
             Blog.objects.create(
                 title=blog_title,
-                content=blog_content
+                content=blog_content,
+                author=request.user
             )
 
     return redirect('/')
@@ -35,7 +37,7 @@ def flushBlogsView(request):
     return redirect('/')
 
 def loginView(request):
-    """Render the login view"""
+    """Render the login form"""
     
     return render(request, 'loginpage.html')
 
@@ -47,8 +49,8 @@ def logoutView(request):
     return redirect('/login')
 
 def createAccountView(request):
-    """Render the form for creating a new user"""
-    
+    """Render account creation form"""
+
     return render(request, 'registerpage.html')
 
 def attemptedLoginView(request):
@@ -71,5 +73,14 @@ def saveNewUserView(request):
         username=username,
         password=password
     )
+
+    return redirect('/')
+
+def deleteUserView(request, pk):
+    """Delete a user"""
+
+    remove_this_user = User.objects.get(id=pk)
+    print(remove_this_user)
+    remove_this_user.delete()
 
     return redirect('/')
